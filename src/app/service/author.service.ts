e5f85a9a27b22eaeeb362ab6c77e267554669ae9
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-06-09 14:45:19 +0400

    💚 ci (methods): create findById and updateAuthorFields methods

--- src/app/service/author.service.ts
+++ src/app/service/author.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Author} from '../model/author';
-import {catchError, Observable, Subject} from "rxjs";
+import {catchError, Observable, Subject, tap} from "rxjs";
 import {environment} from "../../environments/environment";
 import {ErrorHandlerService} from "./error-handler.service";
 
@@ -17,18 +17,34 @@ export class AuthorService {
     return this.http.get<Author[]>(this.authorsUrl);
   };
 
+  public findById(id: number): Observable<Author> {
+    const URL = `${this.authorsUrl}/author/${id}`;
+
+    return this.http.get<Author>(URL).pipe(
+      catchError(this.errorHandlerService.handleError<Author>(`findAuthor id=${id}`)));
+  }
+
+
   public addAuthor(author: Author): Observable<Author> {  // The method returns an Observable that will emit an Author object.
     return this.http.post<Author>(this.authorsUrl + "/post", author)
       .pipe(catchError(this.errorHandlerService.handleError<Author>('addAuthor', author)))
   };
 
+  public updateAuthorFields(id: number, author: Author) {
+    const URL = `${this.authorsUrl}/update/${id}`;
+
+    return this.http.patch(URL, author).pipe(
+      catchError(this.errorHandlerService.handleError('patchAuthor'))
+    );
+  };
+
   public deleteAuthor(id: number): Observable<unknown> {
-    const url = `${this.authorsUrl}/delete/${id}`;
-    console.log("deleteAuthor ID: " + id + " url " + url);
-    return this.http.delete(url).pipe(
+    const URL = `${this.authorsUrl}/delete/${id}`; //@TODO: remove string
+    console.log("deleteAuthor ID: " + id + " url " + URL);
+    return this.http.delete(URL).pipe(
       catchError(this.errorHandlerService.handleError('deleteAuthor'))
     );
-  }
+  };
 
 }
 
