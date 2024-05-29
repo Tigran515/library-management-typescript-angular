7544eef11786fa463b4a07d348b6a29d078a645c
Tigran Karapetyan <tigrankarapetyana@gmail.com>
2024-05-29 18:22:12 +0400

    ci 💚 (search) implement search methods

--- src/app/service/search.service.ts
+++ src/app/service/search.service.ts
@@ -1,22 +1,45 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {ErrorHandlerService} from "./error-handler.service";
 import {environment} from "../../environments/environment";
 import {SearchCriteriaDto} from "../dto/search-criteria-dto";
-import {catchError} from "rxjs";
+import {catchError, Observable} from "rxjs";
+import {addIfNotNull} from "../shared/utils/http-param-utils";
+import {BookAuthorDTO} from "../dto/bookAuthor.dto";
+import {Author} from "../model/author";
 
 @Injectable()
 export class SearchService {
   public searchURL: string;
 
   constructor(private http: HttpClient, private errorHandlerService: ErrorHandlerService) {
-    this.searchURL = environment.server.HOST + "/bookAuthor/search"
+    this.searchURL = environment.server.HOST + "/bookAuthor/search";
+
   };
 
-  public findByTerm() {
-    // return this.http.post<any>(this.searchURL, searchCriteria)
-    // return this.http.get<any>(this.searchURL+"/leo")
-    //   .pipe(catchError(this.errorHandlerService.handleError<any>("find by term")))
+  public searchByTerm(searchCriteria: SearchCriteriaDto): Observable<BookAuthorDTO[]> {
+    const URL: string = `${this.searchURL}/${searchCriteria.getDetail}`;
+    return this.http.get<BookAuthorDTO[]>(URL).pipe(
+      catchError(this.errorHandlerService.handleError<BookAuthorDTO[]>(`findAuthor id=${searchCriteria.getDetail}`)));
+  }
+
+  public advancedSearch(searchCriteria: SearchCriteriaDto): Observable<BookAuthorDTO[]> { //FIXME: rename & change any to current obj type
+    let params: HttpParams = new HttpParams();
+    Object.keys(searchCriteria).forEach(key => {
+      params = addIfNotNull(params, key.substring(1), (searchCriteria as any)[key]);
+    })
+    return this.http.get<BookAuthorDTO[]>(this.searchURL, {params: params}) // @FIXME: change any to current obj type
+      .pipe(catchError(this.errorHandlerService.handleError<BookAuthorDTO[]>("find by term")))
   }
 
 }
+
+
+// public advancedSearch(searchCriteria: SearchCriteriaDto): Observable<any> { //FIXME: rename & change any to current obj type
+//   let params: HttpParams = new HttpParams();
+//   Object.keys(searchCriteria).forEach(key => {
+//     params = addIfNotNull(params, key.substring(1), (searchCriteria as any)[key]);
+//   })
+//   return this.http.get<any>(this.searchURL, {params: params}) // @FIXME: change any to current obj type
+//     .pipe(catchError(this.errorHandlerService.handleError<any>("find by term")))
+// }
